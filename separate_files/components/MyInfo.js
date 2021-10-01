081081a141a73419a8bce7989d50193ed55a330b
Timothée Thiry <timothee.thiry@gmail.com>
2021-10-01 15:36:09 +0200

    feat: components into separate files

---
+++ separate_files/components/MyInfo.js
@@ -0,0 +1,18 @@
+import React from "react"; // we need to import the react library to read the code as JSX
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Timothée Thiry</h1>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Places Id' like to go to for the holidays :</p>
+            <ul>
+                <li>Sicilia - Taormina, Etna</li>
+                <li>Yosemite National Park</li>
+                <li>New Zealand</li>
+            </ul>
+        </div>
+    )
+};
+
+export default MyInfo;
---
+++ separate_files/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react";
+
+import MyInfo from "./components/MyInfo";
+
+ReactDOM.render(
+    <MyInfo />,
+    document.getElementById("root")
+);
