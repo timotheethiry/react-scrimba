65adea07a92e2610f56b7892a77f8dbd01d12c84
Timothée Thiry <timothee.thiry@gmail.com>
2021-10-01 16:21:07 +0200

    feat: parent / child components

---
+++ parent_child_components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//components
+import Header from "./components/Header";
+import HelloWorld from "./components/HelloWorld";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <HelloWorld />
+        </div>
+    )
+};
+
+export default App;
---
+++ parent_child_components/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <img id="logo" />
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact us</li>
+                </ul>
+            </nav>
+        </header>
+    )
+};
+
+export default Header;
