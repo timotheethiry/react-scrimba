040584761e669de0fed4057b1e3c2fa69078d825
Timothée Thiry <timothee.thiry@gmail.com>
2021-10-06 17:10:37 +0200

    ToDo app phase 2

---
+++ todo-app-phase2/components/ToDoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ToDoItem() {
+    return (
+        <div className="todo-item">
+            <input type="checkbox" id="phase1" name="phase1" />
+            <label for="phase1">To Do app -- phase 1</label>
+        </div>
+    )
+};
+
+export default ToDoItem;
---
+++ todo-app-phase2/components/ToDoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ToDoItem from "./ToDoItem";
+
+function ToDoList() {
+  return (
+    <form>
+      <h2>Learning React with Scrimba</h2>
+      <ToDoItem />
+    </form>
+  );
+}
+
+export default ToDoList;
