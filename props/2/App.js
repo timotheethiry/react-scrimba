ecd90948016dc82a0a0d45b9a4ead120f3dc974c
Timothée Thiry <timothee.thiry@gmail.com>
2021-10-08 23:14:51 +0200

    props and styling

--- props/2/App.js
+++ props/2/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import ContactCard from "./ContactCard";
 
 function App() {
-    let user4 = {name="Monica", phone="951", email="monica.bing", imgUrl=""};
-    
+    let user4 = {name:"Monica", phone:"951", email:"monica.bing", imgUrl:""};
+
     return (
         <div>
-            <ContactCard contact={{name="Joey", phone="123", email="joey.tribbiani", imgUrl=""}} />
-            <ContactCard contact={{name="Chandler", phone="456", email="chandler.bing", imgUrl=""}} />
-            <ContactCard contact={{name="Ross", phone="789", email="ross.geller", imgUrl=""}} />
+            <ContactCard contact={{name:"Joey", phone:"123", email:"joey.tribbiani", imgUrl:""}} />
+            <ContactCard contact={{name:"Chandler", phone:"456", email:"chandler.bing", imgUrl:""}} />
+            <ContactCard contact={{name:"Ross", phone:"789", email:"ross.geller", imgUrl:""}} />
             <ContactCard contact={user4} />
         </div>
     )
